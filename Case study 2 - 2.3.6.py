e578ea7e19f0f4e6b74d813c4c33598011fd6a19
JonatanMDH <57435349+JonatanMDH@users.noreply.github.com>
2019-11-07 10:07:28 +0100

    Add files via upload

---
+++ Case study 2 - 2.3.6.py
@@ -0,0 +1,21 @@
+# Importing some weird add-ons
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# Mass is not defined, so we need to make up multiple masses
+mass = np.linspace(0, 15, 100)    
+
+# Formula to calculate the required power
+power_req = 0.29 * mass**(7/6)
+power_av = mass**(2/3)
+
+
+# Plotting the graph and adding labels
+plt.plot(mass, power_req, label="Required power")
+plt.plot(mass, power_av, label="Available power")
+plt.xlabel("Mass (kg)")
+plt.ylabel("Required power (Watt)")
+plt.title("Some weird flying bird")
+plt.grid()
+plt.legend()
